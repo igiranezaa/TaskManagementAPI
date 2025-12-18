bb32a6727ea528f198315c1958acffd61b37b63c
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2025-12-18 11:55:56 +0200

    Fix Gunicorn WSGI path and build script for Render

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 set -o errexit
 
 pip install -r requirements.txt
-python task_api_project/manage.py collectstatic --noinput
-python task_api_project/manage.py migrate
+
+cd task_api_project
+
+python manage.py collectstatic --noinput
+python manage.py migrate
